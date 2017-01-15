97c34abcfe7060b40989d6c53976151aacf661ca
brycexu <XuXianda@xuxiandadeMBP.lan>
2017-01-15 21:17:49 +0800

    My Diary
    把框架搭了一下，其余的再慢慢研究[汗]

---
+++ My Diary/My Diary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My Diary
+//
+//  Created by 徐贤达 on 2017/1/15.
+//  Copyright © 2017年 徐贤达. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
