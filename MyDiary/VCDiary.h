1039aa7fcaadfa63236491f4aeba80749a7a86ef
SowardAndTea <xiangweiqaz@gmail.com>
2017-01-20 18:05:55 -0800

    built the framework of the calendar project

---
+++ MyDiary/VCDiary.h
@@ -0,0 +1,15 @@
+//
+//  VCDiary.h
+//  MyDairy
+//
+//  Created by 向尉 on 2017/1/15.
+//  Copyright © 2017年 向尉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface VCDiary : ViewController
+
+
+@end
---
+++ MyDiary/VCDiaryWrite.m
@@ -0,0 +1,38 @@
+//
+//  VCDiaryWrite.m
+//  MyDairy
+//
+//  Created by 向尉 on 2017/1/18.
+//  Copyright © 2017年 向尉. All rights reserved.
+//
+
+#import "VCDiaryWrite.h"
+
+@interface VCDiaryWrite ()
+
+@end
+
+@implementation VCDiaryWrite
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
