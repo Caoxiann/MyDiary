e4dc05686c298c1f0b9d8717423524790fd6c229
SowardAndTea <xiangweiqaz@gmail.com>
2017-01-20 18:52:01 -0800

    Initial Commit

---
+++ MyDiary02/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyDiary02
+//
+//  Created by 向尉 on 2017/1/20.
+//  Copyright © 2017年 向尉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyDiary02/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyDiary02
+//
+//  Created by 向尉 on 2017/1/20.
+//  Copyright © 2017年 向尉. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
