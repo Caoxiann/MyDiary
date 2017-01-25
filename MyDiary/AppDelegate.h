84b4e351a9ee8b98af643bd41972b51ada8d0af0
Jimmy Fan <fjm@JimmydeMacBook-Air.local>
2017-01-25 18:56:17 +0800

    第一次提交

---
+++ MyDiary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDiary
+//
+//  Created by Jimmy Fan on 2017/1/22.
+//  Copyright © 2017年 Jimmy Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDiary/VCCalendar.h
@@ -0,0 +1,13 @@
+//
+//  VCCalendar.h
+//  MyDiary
+//
+//  Created by Jimmy Fan on 2017/1/22.
+//  Copyright © 2017年 Jimmy Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCCalendar : UIViewController
+
+@end
